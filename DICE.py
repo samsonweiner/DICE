7d4a7e8c065c83809973ba13dc1ecc847eb36ba0
samsonweiner <samson.weiner@uconn.edu>
2023-10-03 23:56:32 -0400

    First working version of DICE.

---
+++ DICE.py
@@ -0,0 +1,221 @@
+# Copyright (C) 2023 Samson Weiner (samson.weiner@uconn.edu) and
+# Mukul S. Bansal (mukul.bansal@uconn.edu).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+
+import os
+import subprocess
+import argparse
+import time
+import sys
+
+import pandas as pd
+import numpy as np
+
+np.set_printoptions(threshold=sys.maxsize)
+
+# Read data into pandas dataframe
+def process_data(data_path, use_total):
+    df = pd.read_csv(data_path, delimiter='\t')
+    df = df.set_index(['CELL', 'chrom', 'start'])
+    df = df.drop(columns=['end'])
+    df = df.sort_index()
+    if use_total:
+        df.columns = [0]
+    else:
+        df = df['CN states'].str.split(',', expand=True)
+    df = df.astype('int32')
+    return df
+
+# Manhattan distance between rows a and b.
+def distance_manhattan(a, b):
+    return np.sum(np.abs(np.subtract(a, b)))
+
+# Euclidean distance between rows a and b.
+def distance_euclidean(a, b):
+    return np.dot(a,a) - 2*np.dot(a,b) + np.dot(b,b)
+
+# Root distance between rows a and b.
+def distance_root(a, b):
+    return np.sum(np.sqrt(np.abs(np.subtract(a,b))))
+
+# Log distance between rows a and b
+def distance_log(a, b):
+    return np.sum(np.log(np.abs(np.subtract(a, b)) + 1))
+
+# Computes cell by cell distance matrix.
+def compute_distmatrix(prof_df, use_breakpoint, metric, use_total):
+    cell_names = prof_df.index.get_level_values(0).drop_duplicates().to_numpy()
+    chrom_names = prof_df.index.get_level_values(1).drop_duplicates().to_numpy()
+    n = cell_names.shape[0]
+
+    dist_matrix = np.zeros((n, n))
+
+    for chrom in chrom_names:
+        if use_total:
+            alleles = [0]
+        else:
+            alleles = [0, 1]
+        for allele in alleles:
+            cur_prof = prof_df.xs(chrom, level='chrom')[allele]
+            cur_prof = cur_prof.unstack(level='start').to_numpy()
+            if use_breakpoint:
+                cur_prof = np.apply_along_axis(np.ediff1d, 1, cur_prof)
+
+            for i in range(cell_names.size - 1):
+                for j in range(i+1, cell_names.size):
+                    dist_matrix[i,j] += metric(cur_prof[i], cur_prof[j])
+    if metric == distance_euclidean:
+        dist_matrix = np.sqrt(dist_matrix)
+
+    dist_matrix = dist_matrix + dist_matrix.T - np.diag(np.diag(dist_matrix))
+    return dist_matrix, cell_names
+
+# Saves the distance matrix in PHYLIP format
+def save_distmatrix(dist_matrix, cell_names, file_name):
+    n = len(cell_names)
+    max_char = max([len(cell) for cell in cell_names]) + 1
+    with open(file_name, 'w+') as f:
+        f.write(str(n) + '\n')
+        for i in range(n):
+            cell = cell_names[i]
+            x = np.array2string(dist_matrix[i], formatter={'float_kind':lambda x: "%.5f" % x})[1:-1].replace('\n', '')
+            sep_char = ' ' * (max_char-len(cell))
+            f.write(cell + sep_char + x + '\n')
+
+# Build the tree using the balanced ME of fastme
+def construct_tree_fast(file_name, save_dm, tree_prefix, method, use_NNI, fastme_path, seed):
+    if method == 'fastNJ':
+        flags = ['N']
+    elif method == 'fastuNJ':
+        flags = ['U']
+    elif method == 'balME':
+        if use_NNI:
+            flags = ['B', '-n']
+        else:
+            flags = ['B', '-s']
+    elif method == 'olsME':
+        if use_NNI:
+            flags = ['O', '-n']
+        else:
+            flags = ['O', '-s']
+    if seed:
+        flags += ['-z', str(seed)]
+    call = subprocess.run([fastme_path, '-i', file_name, '-o', tree_prefix, '-m'] + flags, capture_output=True, text=True)
+    if not save_dm:
+        os.remove(file_name)
+
+metrics = {
+    'manhattan': distance_manhattan,
+    'euclidean': distance_euclidean,
+    'root': distance_root,
+    'log': distance_log
+}
+
+def main(in_path, out_path, prefix, save_dm, rec_method, use_breakpoint, dist_type, use_total, use_NNI, fastme_path, seed):
+    start_time = time.time()
+    part1_time = part2_time = part3_time = None
+
+    if not out_path:
+        out_path = os.getcwd()
+    if not os.path.isdir(out_path):
+        os.makedirs(out_path)
+    if dist_type not in list(metrics.keys()):
+        print('Invalid distance type.')
+        return
+    
+    if not prefix:
+        if use_breakpoint:
+            prefix = f'breakpoint_{dist_type}'
+        else:
+            prefix = f'standard_{dist_type}'
+        if rec_method:
+            prefix += f'_{rec_method}'
+            if rec_method == 'balME' or rec_method == 'olsME':
+                if use_NNI:
+                    prefix += '_nni'
+
+    print('Reading data...', end="", flush=True)
+    if in_path[-4:] == '.tsv':
+        profiles_df = process_data(in_path, use_total)
+        print('Done')
+        part1_time = time.time()
+        print('Computing distance matrix...', end="", flush=True)
+        dist_matrix, cell_names = compute_distmatrix(profiles_df, use_breakpoint, metrics[dist_type], use_total)
+        dm_prefix = os.path.join(out_path, prefix + '_dm.phy')
+        save_distmatrix(dist_matrix, cell_names, dm_prefix)
+        print('Done')
+        part2_time = time.time()
+    elif in_path[-4:] == '.phy':
+        dm_prefix = in_path
+        save_dm = True
+        print('Done')
+        part1_time = start_time
+    if rec_method:
+        print('Constructing tree...', end="", flush=True)
+        if rec_method in ['NJ', 'uNJ', 'balME', 'olsME']:
+            tree_prefix = os.path.join(out_path, prefix + '_tree.nwk')
+            construct_tree_fast(dm_prefix, save_dm, tree_prefix, rec_method, use_NNI, fastme_path, seed)
+            part3_time = time.time()
+        else:
+            print('Invalid reconstruction method.')
+            return
+    print('Done')
+
+    info_prefix = os.path.join(out_path, prefix + '_info.txt')
+        
+    with open(info_prefix, 'w+') as f:
+        f.write('Distance: ' + '\t' + dist_type + '\n')
+        if rec_method:
+            f.write('Method: ' + '\t' + rec_method + '\n')
+        else:
+            f.write('Method: ' + '\t' + 'None' + '\n')
+        f.write('Breakpoint: ' + '\t' + str(use_breakpoint) + '\n')
+        f.write('Total copy numbers: ' + '\t' + str(use_total) + '\n')
+        if in_path[-4:] != '.phy':
+            f.write('Reading time elapsed: ' + '\t' + str(part1_time - start_time) + '\n')
+        if part2_time:
+            f.write('Dm computation time elapsed: ' + '\t' + str(part2_time - part1_time) + '\n')
+            if part3_time:
+                f.write('Tree construction time elapsed: ' + '\t' + str(part3_time - part2_time) + '\n')
+        else:
+            f.write('Tree construction time elapsed: ' + '\t' + str(part3_time - part1_time) + '\n')
+        f.write('Total time elapsed: ' + '\t' + str(time.time() - start_time) + '\n')
+        
+    if rec_method != None:
+        f1, f2 = open(info_prefix, 'a+'), open(dm_prefix + '_fastme_stat.txt', 'r')
+        f1.write('\n\nfastme stats\n')
+        f1.write(f2.read())
+        f1.seek(0)
+        f1.close()
+        f2.close()
+        os.remove(dm_prefix + '_fastme_stat.txt')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', type=str, required=True, help='Path to dataset.')
+parser.add_argument('-o', '--output', type=str, default=None, help='Output directory. Will create one if it does not exist. Default is current directory.')
+parser.add_argument('-p', '--prefix', type=str, default=None, help='Prefix to add to output files.')
+parser.add_argument('-s', '--save-dm', action='store_true', help='Toggle to save the distance matrix to a file.')
+parser.add_argument('-b', '--breakpoint', action='store_true', help='Toggle to use breakpoint profiles.')
+parser.add_argument('-t', '--total-cn', action='store_true', help='Use total copy numbers instead of allele-specific copy numbers.')
+parser.add_argument('-d', '--dist-type', type=str, default='root', help='Distance measure type.')
+parser.add_argument('-m', '--rec-method', type=str, default=None, help='Phylogenetic reconstruction algorithm. If not specified, will compute the distance matrix and save to a file. Options are \'fastNJ\', \'fastuNJ\', \'balME\', \'olsME\'.')
+parser.add_argument('-n', '--use-NNI', action='store_true', help='For ME methods, toggle to use NNI tree search. By default, SPR tree search is used.')
+parser.add_argument('-f', '--fastme-path', type=str, default='fastme', help='Path to \'fastme\' executable. By default, assumes the fastme executable is added to the user $PATH and is called directly.')
+parser.add_argument('-z', '--seed', type=int, default=None, help='Randomization seed used in fastme.')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    main(args.input, args.output, args.prefix, args.save_dm, args.rec_method, args.breakpoint, args.dist_type, args.total_cn, args.use_NNI, args.fastme_path, args.seed)
--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# DICE
-DICE: Fast and Accurate Distance-Based Reconstruction of Single-Cell Copy Number Phylogenies
+﻿# DICE
+
+## **Description**
+DICE (Distance-based Inference of Copy-number Evolution) is a collection of simple and efficient methods for reconstructing cell lineage trees from single-cell copy number aberration data. Most notable among these methods are DICE-star and DICE-bar, which use the standard and breakpoint root distances, respectively, and reconstruct the phylogeny using a balanced minimum evolution criteria. Both approaches, and many variants, are implemented in a single Python file and can be easily run using a python interpreter. DICE can be cited as follows:
+
+<a>DICE: Fast and Accurate Distance-Based Reconstruction of Single-Cell Copy Number Phylogenies</a><br>
+Samson Weiner and Mukul S. Bansal<br>
+Under review.
+
+## Installation
+If on github, you can clone the this repository with
+```
+git clone https://github.com/samsonweiner/DICE.git
+```
+Otherwise, the source code can be downloaded at https://compbio.engr.uconn.edu/software/dice/.
+
+### Dependencies
+
+The following python packages are required to run the software:
+* [Numpy](https://numpy.org/)
+* [Pandas](https://pandas.pydata.org/)
+
+Additionally, DICE requires the [fastme](http://www.atgc-montpellier.fr/fastme/binaries.php) package. The easiest and recommended approach to install fastme is with `conda` (see [here](https://anaconda.org/bioconda/fastme)). Otherwise, users can download existing executables from the website. In this case, it is recommended that the executable be added to the user’s `$PATH` variable. 
+
+The commands used for a full install with `conda` following best practices are shown below.
+```
+conda create -n DICE python=3
+conda activate DICE
+
+conda install numpy
+conda install pandas
+conda install -c bioconda fastme
+```
+
+## Usage
+
+Running DICE under default parameter settings will use the DICE-star method (standard root distance) and save the distance matrix to a file. The only required input is the path to the file containing the copy number profiles. To run DICE-star with the balanced ME phylogenetic reconstruction algorithm, use the command
+```
+python3 DICE.py -i inputProfiles.tsv -o outputDir -m balME
+```
+To run DICE-bar with balanced ME, use the same command with the `-b` flag:
+```
+python3 DICE.py -i inputProfiles.tsv -o outputDir -m balME -b
+```
+
+**Input File Format:** DICE** takes as input a single file (specified using the –i command line option) containing a tab-separated values (TSV) file describing the copy number profiles of all cells. The following headers are required for each file and should be placed on the first line: CELL (the cell id of the current row), chrom (the chromosome **X** of the current row in the form of **chrX**), start (the starting location in bp of the copy number bin), end (the ending location in bp of the copy number bin), CN states (the actual copy number of the bin in the current row). If total copy numbers are used, the value of CN states should be a single numerical value. If allele-specific copy numbers are used, the value of CN states should be **a,b** where **a** is the copy number for haplotype A, and **b** is the copy number for haplotype B.
+
+E.g.
+
+CELL &nbsp; chrom &nbsp; start &nbsp;&nbsp;&nbsp;&nbsp; end &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CN states <br>
+leaf1 &nbsp;&nbsp; chr1	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 0 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 10000 &nbsp;&nbsp;&nbsp; 1,1 <br>
+leaf2 &nbsp;&nbsp; chr1	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 0 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 10000 &nbsp;&nbsp;&nbsp; 1,2 <br>
+leaf5 &nbsp;&nbsp; chr3 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;50000 &nbsp;60000	&nbsp;&nbsp;&nbsp;3,4
+
+See the attached sample input file for a full example.
+
+
+## Available command line options for DICE
+`-i, --input` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Path to input file (Required).
+
+`-o, --output` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Path to output directory (Default: current directory). 
+
+`-p, --prefix` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Prefix to add to output files (Default: DICE variant). 
+
+`-s, --save-dm` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Toggle to save distance matrix to file in PHYLIP format (Default: False). 
+
+`-b,--breakpoint` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Toggle to use breakpoint profiles. Otherwise, uses standard profiles (Default: False).
+
+`-t,--total-cn` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Toggle to use total copy numbers. Otherwise, assumes allele-specific copy numbers (Default: False).
+
+`-d,--dist-type` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Choice of distance function. Options are *root*, *log*, *euclidean*, and *manhattan*. (Default: root).
+
+`-m,--rec-method` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Choice of phylogenetic reconstruction algorithm. Options are *balME*, *olsME*, *NJ*, and *uNJ*. If not specified, computes pairwise distances and saves to a file. (Default: None).
+
+`-n,--use-NNI` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Toggle to use NNI tree search. Otherwise, uses an SPR tree search. (Default: root).
+
+`-f,--fastme-path` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Path to the fastme executable. (Default: fastme).
+
+`-z,--seed` &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; RNG seed used in fastme. (Default: None).
+
+
+## Available distance measures
+
+Currently there are 4 distance measures available: manhattan, euclidean, log, and root. The breakpoint variants of these measures can be achieved by using the `-b` toggle. The root and log distances are defined as taking the square root and logarithm of each term in the manhattan distance, respectively. DICE-star and DICE-bar both use the root distance, as it has been shown to perform the best among all four.
+
+## Available Reconstruction algorithms
+
+DICE makes available the four phylogenetic reconstruction algorithms implemented in fastme for computing a tree from a distance matrix. These are: balanced minimum evolution (balME), ordinary least-squares minimum evolution (olsME), neighbor-joining (NJ), and unweighted neigbor-joining (uNJ). For more information on these algorithms, see the fastme documentation.
+
+
+## Contact
+If there are any questions related to DICE, please contact Samson Weiner (<samson.weiner@uconn.edu>) or Mukul Bansal (<mukul.bansal@uconn.edu>), or visit <https://compbio.engr.uconn.edu/> for a complete list of available software and datasets.
+
